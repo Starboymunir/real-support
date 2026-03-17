581a25b6cda0ca9d75737e85ca60b4fb60c2d89f
Starboymunir <71543859+Starboymunir@users.noreply.github.com>
2026-03-17 17:56:41 +0100

    fix: remove i18next-browser-languagedetector dependency

--- src/app/(RSAdmin)/admin/locales/i18n.ts
+++ src/app/(RSAdmin)/admin/locales/i18n.ts
@@ -1,7 +1,6 @@
 "use client";
 
 import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 // utils
 import { localStorageGetItem } from "@/lib/utils/storage-available";
@@ -22,7 +21,6 @@ const lng =
   "en";
 
 i18n
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
